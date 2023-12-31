8945f958db017df208069c7977d9fcc40e1b92c8
suxiang <2585546823@qq.com>
2023-12-31 16:10:24 +0800

    开发用户脱敏和无脱敏信息查询接口

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +21,23 @@ public class UserController {
     private UserService userService;
 
     /**
-     *  根据用户名查找用户信息
+     *  根据用户名查找用户信息:有脱敏信息
      * @param username
      * @return
      */
     @GetMapping("/api/short-link/admin/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
-        Result<UserRespDTO> userRespDTOResult = new Result<>();
         UserRespDTO userRespDTO = userService.getUserByUsername(username);
-        return userRespDTOResult.setCode("0").setData(userRespDTO);
+        return Results.success(userRespDTO);
+    }
+
+    /**
+     *  根据用户名查找用户信息：无脱敏信息
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/short-link/admin/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getUserByActualUsername(@PathVariable("username") String username){
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,14 +1,12 @@
 package com.nageoffer.shortlink.admin.dto.resp;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.nageoffer.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
  * 用户返回参数响应
  */
 @Data
-public class UserRespDTO {
+public class UserActualRespDTO {
 
     private Long id;
 
@@ -30,7 +28,6 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
