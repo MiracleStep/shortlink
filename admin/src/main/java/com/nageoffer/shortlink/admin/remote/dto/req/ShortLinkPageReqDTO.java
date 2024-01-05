f8f48bf7439a47dfbbad1f5a2f0e5c2217171aab
suxiang <2585546823@qq.com>
2024-01-05 17:32:55 +0800

    短连接后管系统调用中台请求接口

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,17 @@
+package com.nageoffer.shortlink.admin.remote.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+
+/**
+ * 短链接分页请求参数
+ */
+@Data
+public class ShortLinkPageReqDTO extends Page {
+    //继承Page去接收请求参数current当前页和size每页显示个数。
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
